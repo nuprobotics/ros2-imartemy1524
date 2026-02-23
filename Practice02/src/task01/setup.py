4c3ac929c863099b1a0f0ca5fa5ebe0b8013890b
im <imartemy1524@gmail.com>
2026-02-23 16:50:27 +0200

    Fix filename issue

--- Practice02/src/task01/setup.py
+++ Practice02/src/task01/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/task1.launch']),
+        ('share/' + package_name + '/launch', ['launch/task01.launch']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
